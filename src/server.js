b9b8140fcee2cd46fd579c970d739720af36b5c1
HanJun Lim <bjj7399@naver.com>
2024-08-27 00:21:27 +0900

    WebSocket 개발 환경 세팅 및 테스트

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 import express from "express";
+import http from "http";
+import WebSocket from "ws";
 
 const app = express();
 
@@ -6,6 +8,20 @@ app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
+//app.get("/*", (req, res) => res.render("home"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
-app.listen(3000, handleListen);
+
+const server = http.createServer(app);          // http 서버
+const wss = new WebSocket.Server({server});     // wss 서버
+
+wss.on("connection", (socket) => {
+    console.log("Connected to Browser");
+    socket.on("close", () => console.log("Disconnected from Browser"));
+    socket.on("message", (message) => {
+        console.log(message.toString("utf-8"));
+    });
+    socket.send("hello");
+});
+
+server.listen(3000, handleListen);      // 동일한 포트에서 양 서버 지원
